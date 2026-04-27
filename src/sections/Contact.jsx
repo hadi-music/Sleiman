dd0b5b13bf52abe1e85d9721b3d6a785c2e0341e
Hadi <hxdibexts@gmail.com>
2026-04-27 11:43:32 +0300

    press page clean up + contact title change

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -4,7 +4,7 @@ import "./contact.css";
 
 export default function Contact() {
   return (
-    <Section headerTitle="Contact" className="contact">
+    <Section headerTitle="GET IN TOUCH" className="contact">
       <div className="contact-svg-wrapper">
         <img src="/images/contact.svg" alt="Contact" className="contact-svg" />
       </div>
